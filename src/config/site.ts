16cd7084a2a7076e0e2b292a97b439808f2ca758
rci-prog <rci.protocol00@gmail.com>
2026-05-11 11:07:49 -0300

    Create site.ts

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const SITE_CONFIG = {
+  url: 'https://www.registai.com.br',
+  name: 'registAI',
+  tagline: 'Inteligencia Artificial aplicada ao Compliance e Automacao',
+  location: 'Belo Horizonte, MG',
+  country: 'Brasil',
+  supportEmail: 'suporte@registai.com.br',
+  version: '2026.05.11',
+} as const;
