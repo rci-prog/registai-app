4ece26b7d0cb7aed3e254fc3dad23a7d35225d61
rci-prog <rci.protocol00@gmail.com>
2026-05-11 12:10:22 -0300

    Delete src/config directory

--- src/config/site.ts
+++
@@ -1,9 +0,0 @@
-export const SITE_CONFIG = {
-  url: 'https://www.registai.com.br',
-  name: 'registAI',
-  tagline: 'Inteligencia Artificial aplicada ao Compliance e Automacao',
-  location: 'Belo Horizonte, MG',
-  country: 'Brasil',
-  supportEmail: 'suporte@registai.com.br',
-  version: '2026.05.11',
-} as const;
