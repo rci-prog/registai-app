5e8d07cf893243fc776b1d62e504b1b103d83ee8
rci-prog <rci.protocol00@gmail.com>
2026-05-11 21:40:26 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ActivityProvider } from './contexts/ActivityContext';
 import { Toaster } from './components/ui/sonner';
-import Dashboard from '@/components/Dashboard';
+import { Dashboard } from './components/Dashboard'; // Note as { } em volta
 
 function App() {
   return (
