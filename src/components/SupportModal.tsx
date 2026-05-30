4b2a0947b3b6228923fa4e2f9c3fdb48220af77e
rci-prog <rci.protocol00@gmail.com>
2026-05-30 02:59:27 -0300

    Update SupportModal.tsx

--- src/components/SupportModal.tsx
+++ src/components/SupportModal.tsx
@@ -40,7 +40,7 @@ export function SupportModal({ open, onClose }: SupportModalProps) {
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2 text-white">
             <Mail className="w-5 h-5 text-violet-400" />
-            Suporte registAI
+            Suporte RegistAI
           </DialogTitle>
         </DialogHeader>
 
